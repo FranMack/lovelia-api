c7583a463f615ba1f2a66cf9dcadef389731515b
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2025-03-08 14:55:21 -0300

    FIX: fix the google cloud private key format in heroku

--- googleCloudCredentials.js
+++ googleCloudCredentials.js
@@ -1,19 +1,17 @@
-const { envs } = require("./config/env.config")
+const { envs } = require("./config/env.config");
 
- const googleCloudCredentials  ={
-    
-    type : envs.GOOGLE_CLOUD_TYPE,
-    project_id : envs.GOOGLE_CLOUD_PROJECT_ID,
-    private_key_id : envs.GOOGLE_CLOUD_PRIVATE_KEY_ID,
-    private_key :envs.GOOGLE_CLOUD_PRIVATE_KEY,
-    client_email : envs.GOOGLE_CLOUD_CLIENT_EMAIL,
-    client_id : envs.GOOGLE_CLOUD_CLIENT_ID,
-    auth_uri : envs.GOOGLE_CLOUD_AUTH_URI,
-    token_uri : envs.GOOGLE_CLOUD_TOKEN_URI,
-    auth_provider_x509_cert_url : envs.GOOGLE_CLOUD_UNIVERSE_DOMAIN,
-    client_x509_cert_url : envs.GOOGLE_CLOUD_CLIENT_X509_CERT_URL,
-    universe_domain : envs.GOOGLE_CLOUD_UNIVERSE_DOMAIN,
-}
+const googleCloudCredentials = {
+  type: envs.GOOGLE_CLOUD_TYPE,
+  project_id: envs.GOOGLE_CLOUD_PROJECT_ID,
+  private_key_id: envs.GOOGLE_CLOUD_PRIVATE_KEY_ID,
+  private_key: envs.GOOGLE_CLOUD_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: envs.GOOGLE_CLOUD_CLIENT_EMAIL,
+  client_id: envs.GOOGLE_CLOUD_CLIENT_ID,
+  auth_uri: envs.GOOGLE_CLOUD_AUTH_URI,
+  token_uri: envs.GOOGLE_CLOUD_TOKEN_URI,
+  auth_provider_x509_cert_url: envs.GOOGLE_CLOUD_UNIVERSE_DOMAIN,
+  client_x509_cert_url: envs.GOOGLE_CLOUD_CLIENT_X509_CERT_URL,
+  universe_domain: envs.GOOGLE_CLOUD_UNIVERSE_DOMAIN,
+};
 
-module.exports={googleCloudCredentials }
-
+module.exports = { googleCloudCredentials };
